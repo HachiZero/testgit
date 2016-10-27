540d2284e86861c3f0a120514f6361e9eea9d665
MAC <All@MACui-MacBook-Pro.local>
2016-10-27 16:30:18 +0900

    변경

--- src/pack/test.java
+++ src/pack/test.java
@@ -4,7 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		
 		String git = "안녕? 나는 깃이라고 해 ~ ";
-		String version = "1.0";
+		String version = "1.2";
+		
 		
 		System.out.println(git+" "+"version : ["+version+"]");
 		
