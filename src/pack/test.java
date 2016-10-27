df0455a67b30ac40289b6af5e4825cbcc2aade57
hachizero <helpme@i-um.net>
2016-10-27 15:53:30 -0700

    1.0

---
+++ src/pack/test.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class test {
+	public static void main(String[] args) {
+		
+		String git = "안녕? 나는 깃이라고 해 ~ ";
+		String version = "1.0";
+		
+		System.out.println(git+" "+"version : ["+version+"]");
+		
+	}
+}
